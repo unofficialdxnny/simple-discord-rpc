4a7103cab942a4e965acfa538a3c2d7a21c073ba
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-03-01 20:03:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,22 @@ details = input("Type in something for the details (enter if you dont want) : ")
 os.system("cls)
 
 image = input("Type in link for the image (enter if you dont want) : ")
+          
+os.system("cls")
+          
+label1 = input("Type in title for the first button (enter if you dont want) : ")
+          
+os.system("cls")
+          
+label1url = input("Type in link for the first button (enter if you dont want) : ")
 
+os.system("cls")
+          
+label2 = input("Type in title for the image (enter if you dont want) : ")
+
+os.system("cls")
+          
+label1url = input("Type in link for the second button (enter if you dont want) : ")
 
 
 client = int(input("Paste Your Client ID here"))
@@ -46,4 +61,5 @@ RPC = Presence(client_id)  # Initialize the client class
 RPC.connect() # Start the handshake loop
 
 start_time=time.time() 
-RPC.update(state=state, details=details, large_image=image, buttons=[{"label": "unofficialdxnny", "url": "https://github.com/unofficialdxnny/"}, {"label": "unofficialdxnny Server", "url": "https://discord.gg/jm2BFbqb8h"}], start=start_time)
+RPC.update(state=state, details=details, large_image=image, buttons=[{"label": label1, "url": 
+label1url}, {"label": label2, "url": "https://discord.gg/jm2BFbqb8h"}], start=start_time)
